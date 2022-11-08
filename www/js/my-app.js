bc4ee959ed6e2ae90f8e7ddb93bc1c553b75d1fe
Alejo <rojasalejo.ar@gmail.com>
2022-11-08 12:32:45 -0300

    Agregando cosas

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,8 +14,8 @@ var app = new Framework7({
   },
   // Add default routes
   routes: [
-    
-    {path: '/registro/', url: 'registro.html',},
+
+    { path: '/registro/', url: 'registro.html', },
 
   ]
   // ... other parameters
@@ -31,38 +31,38 @@ $$(document).on('deviceready', function () {
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
   // Do something here when page loaded and initialized
-  console.log(e);
+
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
   // Do something here when page with data-name="about" attribute loaded and initialized
   //console.log(e);
   // alert('Hello');
+  //LOGIN//
+
+  // cada un@ pone su magia para recuperar el mail y la clave de un form...
+  var MAIL = document.getElementById("#rMail");
+  var CONTRA = document.getElementById("#rContra");
+
+  /* firebase.auth().signInWithEmailAndPassword(MAIL, CONTRA)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+
+      console.log("Bienvenid@!!! " + MAIL);
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.error(errorCode);
+      console.error(errorMessage);
+    }); */
 
 })
 
-//LOGIN//
-
- // cada un@ pone su magia para recuperar el mail y la clave de un form...
- var MAIL = "elvalor@delmail.com";
- var CONTRA = "1234567890";
-
-firebase.auth().signInWithEmailAndPassword(MAIL, CONTRA)
-.then((userCredential) => {
- // Signed in
- var user = userCredential.user;
-
- console.log("Bienvenid@!!! " + MAIL);
- // ...
-})
-.catch((error) => {
- var errorCode = error.code;
- var errorMessage = error.message;
-
- console.error(errorCode);
-     console.error(errorMessage);
-});
 
 
 
@@ -70,33 +70,6 @@ firebase.auth().signInWithEmailAndPassword(MAIL, CONTRA)
 
 
 
-//REGISTER//
-// cada un@ pone su magia para recuperar el mail y la clave de un form...
-var MAIL = "elvalor@delmail.com";
-var CONTRA = "1234567890";
-
-
-firebase.auth().createUserWithEmailAndPassword(MAIL, CONTRA)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    console.log("Bienvenid@!!! " + MAIL);
-    // ...
-    mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    console.error(errorCode);
-    console.error(errorMessage);
-
-    if (errorCode == "auth/email-already-in-use") {
-        console.error("el mail ya esta usado");
-    }
-
-    // ..
-  });
 
 
 
@@ -104,62 +77,63 @@ firebase.auth().createUserWithEmailAndPassword(MAIL, CONTRA)
 
 
 //BASE DE DATOS
-var MAIL="";
+var MAIL = "";
 
 var db = firebase.firestore();
 var colUsuario = db.collection("Usuarios");
 
 
-$$("#iNICIO").ON("click", fnLogin);
+$$("#iNICIO").on("click", fnLogin);
+
 function fnLogin() {
 
-    MAIL = $$('#lEmail').val();
-    CONTRA = $$('#lPass').val();
+  MAIL = $$('#lEmail').val();
+  CONTRA = $$('#lPass').val();
 
-    firebase.auth().signInWithEmailAndPassword(MAIL, CONTRA)
-      .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
+  firebase.auth().signInWithEmailAndPassword(MAIL, CONTRA)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
 
-        console.log("Bienvenid@!!! " + MAIL);
+      console.log("Bienvenid@!!! " + MAIL);
 
-        claveDeColeccion = MAIL;
+      claveDeColeccion = MAIL;
 
-        var docRef = colUsuario.doc(claveDeColeccion);
+      var docRef = colUsuario.doc(claveDeColeccion);
 
-        docRef.get().then((doc) => {
-            if (doc.exists) {
-                console.log("Document data:", doc.data());
+      docRef.get().then((doc) => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data());
 
-                console.log(doc.id);
-                console.log(doc.data().nombre);
-                console.log(doc.data().rol);
+          console.log(doc.id);
+          console.log(doc.data().nombre);
+          console.log(doc.data().rol);
 
-                if (doc.data().rol == "admin") {
-                    mainView.router.navigate('/panelAdmin/');
-                } else {
-                    mainView.router.navigate('/panelUsuario/');
-                }
-
-
-            } else {
-                // doc.data() will be undefined in this case
-                console.log("No such document!");
-            }
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
+          if (doc.data().rol == "admin") {
+            mainView.router.navigate('/panelAdmin/');
+          } else {
+            mainView.router.navigate('/panelUsuario/');
+          }
 
 
-        // ...
-      })
-      .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        console.error(errorCode);
-            console.error(errorMessage);
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      }).catch((error) => {
+        console.log("Error getting document:", error);
       });
+
+
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.error(errorCode);
+      console.error(errorMessage);
+    });
 
 
 
@@ -169,63 +143,92 @@ function fnLogin() {
 
 
 $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
-  $$("#boton").ON("click", fnRegistro);
+ 
+  $$("#boton").on("click", (e) => fnRegistro(e));
+  //REGISTER//
+  // cada un@ pone su magia para recuperar el mail y la clave de un form...
+  /* var MAIL = document.getElementById("#rMail");
+  var CONTRA = document.getElementById("#rContra");
+
+  firebase.auth().createUserWithEmailAndPassword(MAIL.value, CONTRA.value)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log("Bienvenid@!!! " + MAIL);
+      // ...
+      mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.error(errorCode);
+      console.error(errorMessage);
+
+      if (errorCode == "auth/email-already-in-use") {
+        console.error("el mail ya esta usado");
+      }
+
+      // ..
+    }); */
+
+
+  function fnRegistro(e) {
+    console.log(e);
+    const RMAIL = $$('#rMail').val();
+    const RCONTRA = $$('#rContra').val();
+    RNOMBRE = $$('#rNombre').val();
+    RAPELLIDO = $$('#rApellido').val();
+    console.log(RMAIL + 'mail')
+    console.log(RCONTRA)
+     firebase.auth().createUserWithEmailAndPassword(RMAIL, RCONTRA)
+       .then((userCredential) => {
+         // Signed in
+         var user = userCredential.user;
+         //console.log("Bienvenid@!!! " + MAIL); 
+         // ...
+         //mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
+ 
+ 
+         claveDeColeccion = RMAIL;
+ 
+ 
+         datos = {
+           nombre: NOMBRE,
+           apellido: APELLIDO,
+           rol: "usuario"
+         }
+ 
+         colUsuario.doc(claveDeColeccion).set(datos)
+           .then(() => {
+             console.log("Document successfully written!");
+           })
+           .catch((error) => {
+             console.error("Error writing document: ", error);
+           });
+ 
+ 
+       })
+       .catch((error) => {
+         var errorCode = error.code;
+         var errorMessage = error.message;
+ 
+         console.error(errorCode);
+         console.error(errorMessage);
+ 
+         if (errorCode == "auth/email-already-in-use") {
+           console.error("el mail ya esta usado");
+         }
+ 
+         // ..
+       });
+
+
+
+  }
+
+
+
 
 })
-
-
-function fnRegistro() {
-
-    MAIL = $$('#rMail').val();
-    CONTRA = $$('#rCcontra').val();
-    NOMBRE = $$('#rNombre').val();
-    APELLIDO = $$('#rApellido').val();
-  
-      firebase.auth().createUserWithEmailAndPassword(MAIL, CONTRA)
-        .then((userCredential) => {
-          // Signed in
-          var user = userCredential.user;
-          console.log("Bienvenid@!!! " + MAIL);
-          // ...
-          //mainView.router.navigate('/siguientePantallaDeUsuarioOK/');
-          
-
-          claveDeColeccion = MAIL;
-  
-          
-          datos = {
-              nombre: NOMBRE,
-              apellido: APELLIDO,
-              rol: "usuario"
-          }
-
-          colUsuario.doc(claveDeColeccion).set(datos)
-          .then(() => {
-              console.log("Document successfully written!");
-          })
-          .catch((error) => {
-              console.error("Error writing document: ", error);
-          });
-  
-  
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-  
-          console.error(errorCode);
-          console.error(errorMessage);
-  
-          if (errorCode == "auth/email-already-in-use") {
-              console.error("el mail ya esta usado");
-          }
-  
-          // ..
-        });
-  
-  
-  
-  }
-  
-
 
